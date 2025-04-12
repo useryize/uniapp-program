9ac6fbf0c74486dab52e456a90f22210187e8379
useryize <136076063@qq.com>
2025-04-12 15:28:05 +0800

    开启下拉

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -8,4 +8,10 @@
 import Banner from "./components/Banner";
 import Recommend from "./components/Recommend";
 import Special from "./components/Special";
+import { onPullDownRefresh } from '@dcloudio/uni-app'
+onPullDownRefresh(() => {
+	setTimeout(() => {
+		uni.stopPullDownRefresh()
+	}, 1000)
+})
 </script>
