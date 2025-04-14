12f95b5a05d03ce8eea496a9329892586c6fbc88
useryize <136076063@qq.com>
2025-04-14 09:06:40 +0800

    path

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -5,9 +5,9 @@
 </template>
 
 <script setup>
-import Banner from "./components/Banner";
-import Recommend from "./components/Recommend";
-import Special from "./components/Special";
+import Banner from "./components/Banner/index.vue";
+import Recommend from "./components/Recommend/index.vue";
+import Special from "./components/Special/index.vue";
 import { onPullDownRefresh } from '@dcloudio/uni-app'
 onPullDownRefresh(() => {
 	setTimeout(() => {
