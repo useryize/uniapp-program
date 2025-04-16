580a6b02649ea7a099a92ea170b7c0ea2cb8b95f
useryize <136076063@qq.com>
2025-04-16 16:23:17 +0800

    banner http

--- pages/home/components/Banner/index.vue
+++ pages/home/components/Banner/index.vue
@@ -8,7 +8,7 @@
     indicator-active-color="rgba(255,255,255,.7)"
   >
     <swiper-item class="item" v-for="(item, index) in bannerList" :key="item?.index">
-      <image :src="HOST + item?.url" mode="aspectFit" />
+      <image :src="HOST + item?.url" mode="scaleToFill" />
     </swiper-item>
   </swiper>
 </template>
@@ -36,15 +36,19 @@ onMounted(() => {
 <style lang="scss" scoped>
 .banner {
   box-sizing: border-box;
- 
+
   .item {
-    height: 300rpx;
+    // height: 300rpx;
+    // aspect-ratio: 16/9;
+    // height: (750 * 1080 / 1920)rpx;
     box-sizing: border-box;
     padding: 0 30rpx;
     overflow: hidden;
-    .img {
+
+    image {
       width: 100%;
       height: 100%;
+      border-radius: 8rpx;
     }
   }
 }
