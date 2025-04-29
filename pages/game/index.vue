a7aef683ec708909769703c471343b239827697e
useryize <136076063@qq.com>
2025-04-29 19:56:26 +0800

    画布大小 网格线

--- pages/game/index.vue
+++ pages/game/index.vue
@@ -5,15 +5,54 @@
 </template>
 <script setup>
 import { ref, onMounted } from "vue";
+const canvasNode = ref(null);
+const canvasWidth = ref(null);
+const canvasHeight = ref(null);
+const gridSize = ref(10);
 
-// 绘制游戏
 const drawGame = () => {
   const ctx = uni.createCanvasContext("gameCanvas");
 
-  ctx.draw();
-  console.error(ctx);
-};
+  // 获取 canvas 的宽高和节点信息
+  const query = uni.createSelectorQuery();
+  query
+    .select(".game-canvas")
+    .fields({ node: true, size: true }, (res) => {
+      console.error(res);
+      canvasNode.value = res.node;
+      canvasWidth.value = res.width;
+      canvasHeight.value = res.height;
 
+      // 画布背景
+      ctx.setFillStyle("#f4f4f4");
+      ctx.fillRect(0, 0, canvasWidth.value, canvasHeight.value);
+
+      ctx.setStrokeStyle("rgba(0, 0, 0, 0.06)"); // 修正透明度设置
+      ctx.setLineWidth(1);
+
+      // 垂直网格线
+      const widthCount = Math.ceil(canvasWidth.value / gridSize.value); // 计算垂直线数量
+      for (let i = 0; i <= widthCount; i++) {
+        ctx.beginPath();
+        ctx.moveTo(i * gridSize.value, 0);
+        ctx.lineTo(i * gridSize.value, canvasHeight.value);
+        ctx.stroke();
+      }
+
+      // 水平网格线
+      const heightCount = Math.ceil(canvasHeight.value / gridSize.value); // 计算水平线数量
+      for (let i = 0; i <= heightCount; i++) {
+        // 确保最后一根线绘制
+        ctx.beginPath();
+        ctx.moveTo(0, i * gridSize.value);
+        ctx.lineTo(canvasWidth.value, i * gridSize.value);
+        ctx.stroke();
+      }
+
+      ctx.draw();
+    })
+    .exec();
+};
 onMounted(() => {
   drawGame();
 });
@@ -27,6 +66,8 @@ onMounted(() => {
     width: 100%;
     height: 100%;
     background-color: #f4f4f4;
+    border-radius: 20rpx;
+    overflow: hidden;
   }
 }
 </style>
